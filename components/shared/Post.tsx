a5e04bdf97e0403744d925f0e124c206b1facdb2
TwitoCode <twitodev@gmail.com>
2022-05-28 18:17:23 -0400

    Post modal page

--- components/shared/Post.tsx
+++ components/shared/Post.tsx
@@ -1,9 +1,9 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { PostWithUser } from "@lib/types";
 import formatDate from "@utils/formatDate";
 import formatVotes from "@utils/formatVotes";
 import shortenString from "@utils/shortenString";
 import { useSession } from "next-auth/react";
-import Link from "next/link";
 import { useCallback } from "react";
 import { ArrowDown, ArrowUp } from "react-feather";
 
@@ -45,25 +45,43 @@ export default function Post({ post }: Props) {
 					<ArrowDown />
 				</button>
 			</div>
-			<Link href={`/realm/${post.realm.id}/post?postId=${post.id}`}>
-				<div className="col-span-11 p-4">
-					<div className="flex justify-between items-center">
-						<div className="flex items-center space-x-4">
-							<img src={post.user.image ?? ""} className="w-10 rounded-md" alt={`${post.user.name}'s avatar`} />
-							<h3 className="opacity-50 mb-4 flex items-center">
-								<span className="font-semibold">
-									{isAuthenticated && post.user.name === session?.user?.name ? "Me" : post.user.name}
-								</span>
-							</h3>
+			<a href="#post-modal" className="w-full col-span-11 p-4">
+				<input type="checkbox" id="my-modal-4" className="modal-toggle" />
+				<div className="modal " id="post-modal">
+					<div className="modal-box space-y-10">
+						<div className="flex justify-between items-center">
+							<div className="flex items-center space-x-4">
+								<img src={post.user.image ?? ""} className="w-10 rounded-md" alt={`${post.user.name}'s avatar`} />
+								<h3 className="opacity-50 mb-4 flex items-center">
+									<span className="font-semibold">
+										{isAuthenticated && post.user.name === session?.user?.name ? "Me" : post.user.name}
+									</span>
+								</h3>
+							</div>
+							<h3>{formatDate(post.createdAt.toString())}</h3>
 						</div>
-						<h3>{formatDate(post.createdAt.toString())}</h3>
-					</div>
-					<div className="space-y-2">
-						<h1 className="font-medium">{post.title}</h1>
-						<p>{shortenString(post.content, 300)}</p>
+						<div className="space-y-2">
+							<h1 className="font-medium text-3xl">{post.title}</h1>
+							<p>{post.content}</p>
+						</div>
 					</div>
 				</div>
-			</Link>
+				<div className="flex justify-between items-center">
+					<div className="flex items-center space-x-4">
+						<img src={post.user.image ?? ""} className="w-10 rounded-md" alt={`${post.user.name}'s avatar`} />
+						<h3 className="opacity-50 mb-4 flex items-center">
+							<span className="font-semibold">
+								{isAuthenticated && post.user.name === session?.user?.name ? "Me" : post.user.name}
+							</span>
+						</h3>
+					</div>
+					<h3>{formatDate(post.createdAt.toString())}</h3>
+				</div>
+				<div className="space-y-2">
+					<h1 className="font-medium">{post.title}</h1>
+					<p>{shortenString(post.content, 300)}</p>
+				</div>
+			</a>
 		</div>
 	);
 }
